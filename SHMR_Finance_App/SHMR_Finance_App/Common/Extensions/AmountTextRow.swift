50723c6de69c756f1195010eb15472068d8835c9
MariaChernokoz <me@chernokoz.ru>
2025-07-11 22:42:22 +0300

    Edit and create transaction added.

---
+++ SHMR_Finance_App/SHMR_Finance_App/Common/Extensions/AmountTextRow.swift
@@ -0,0 +1,8 @@
+//
+//  AmountTextRow.swift
+//  SHMR_Finance_App
+//
+//  Created by Chernokoz on 11.07.2025.
+//
+
+import Foundation
---
+++ SHMR_Finance_App/SHMR_Finance_App/Common/Extensions/EditAmountField.swift
@@ -0,0 +1,18 @@
+//
+//  EditAmountField.swift
+//  SHMR_Finance_App
+//
+//  Created by Chernokoz on 11.07.2025.
+//
+
+import SwiftUI
+
+struct EditAmountField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditAmountField()
+}
